f328e7a5439c52b49ce998d5ca9aaf47c33e396a
aldo278 <aantanolopez@linfield.edu>
2026-06-18 21:23:00 -0700

    added .env for good practice

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 // it is good practice to keep all API calls in a separate file that contains all of your API
-// calls so that you can keep all tghe networking operations or stuff related 
+// calls so that you can keep all the networking operations or stuff related 
 // to the API in a seperate file and find it easily when you need to make changes
 
-const API_KEY = "d454b08c586322cc478842d938e50a87";
+const API_KEY = import.meta.env.VITE_API_KEY;  // free api key so no need to secure it
 const BASE_URL = "https://api.themoviedb.org/3";
 
 
